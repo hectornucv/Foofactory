d53e4626849cd4fd18af35ca5b389816ee26472c
alexander <alexanderddking@gmail.com>
2016-09-07 06:46:45 +1000

    remove unused sections and add acf-json folder

---
+++ single.php
@@ -0,0 +1,4 @@
+<?php wp_head(); ?> <?php get_component([ 'template' => 'template/head' ]); ?> </head>
+ <?php get_component([ 'template' => 'needed/header' ]); ?>
+<?php 	get_component([ 'template' => 'needed/simple-page' ]); ?>
+<?php 	get_component([ 'template' => 'needed/footer' ]); wp_footer(); ?>
