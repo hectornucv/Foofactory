96161f0983ca3f0a61e9d93fcec83663ad4c4a1d
Alex <alexanderddking@gmail.com>
2016-09-07 18:17:43 +1000

    add ninja form

--- builder/elements/form.php
+++ builder/elements/form.php
@@ -1,5 +1,6 @@
 <div class=" molecule form <?php echo $vars['class'] ?>">
 		<h2><?php echo $vars["title"]?></h2>
 		<p><?php echo apply_filters('the_content',  $vars["content"]); ?></p>
-    <?php displayGravityForm($vars["form"]) ?>
+		<?phpif ( function_exists( 'ninja_forms_display_form' ) ) { ninja_forms_display_form( $vars["form"] );}
+		?>
 </div>
